ffc4c51799a0fe70a48e36dbd2950f1128f9283c
cyj <cyd5538@naver.com>
2024-03-29 09:45:38 +0900

    Design: nav style justify-around -> justify-between

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,7 +5,7 @@ import { Home } from 'lucide-react'
 const Nav = () => {
   return (
     <header className='z-40 w-full h-14 bg-white fixed justify-center items-center gap-12'>
-      <nav className='container flex justify-around items-center h-14'>
+      <nav className='container flex justify-between items-center h-14 pl-12 pr-12'>
         <h1 className='cursor-pointer text-xl hover:text-sky-500'>
           <Home size={25}/>
         </h1>
