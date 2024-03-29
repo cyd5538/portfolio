2eab80b7399d7f5ab6fb4b4de5c1694519bf3d6b
cyj <cyd5538@naver.com>
2024-03-29 10:37:40 +0900

    Design: nav opacity

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,14 +4,14 @@ import { Home } from 'lucide-react'
 
 const Nav = () => {
   return (
-    <header className='z-40 w-full h-14 bg-white fixed justify-center items-center gap-12'>
+    <header className='z-40 w-full h-14 bg-white opacity-50 fixed justify-center items-center gap-12'>
       <nav className='container flex justify-between items-center h-14 pl-12 pr-12'>
-        <h1 className='cursor-pointer text-xl hover:text-sky-500'>
+        <h1 className='cursor-pointer text-xl hover:text-sky-500 opacity-100'>
           <Home size={25}/>
         </h1>
         <ul className='flex gap-4 sm:gap-8'>
           {menu.map(data => (
-            <li className='hover:text-sky-500 cursor-pointer' key={data.id}>{data.name}</li>
+            <li className='hover:text-sky-500 cursor-pointer opacity-100' key={data.id}>{data.name}</li>
           ))}
         </ul>
       </nav>
