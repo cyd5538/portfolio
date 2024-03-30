e2e5e9b295e3bf0c7741e0790814dde80c629d8e
cyj <cyd5538@naver.com>
2024-03-30 11:52:18 +0900

    Docs: nextConfig image domain add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['skillicons.dev'],
+  },
+};
 export default nextConfig;
