3f14fc57951c3b5fcc4851edf944903072084785
cyj <cyd5538@naver.com>
2024-04-02 09:05:16 +0900

    Feat: project

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About/About";
 import Archives from "@/components/Archives/Archives";
 import Main from "@/components/Home/Home";
 import Nav from "@/components/Nav";
+import Project from "@/components/Project/Project";
 import Skill from "@/components/Skills/Skill";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <About /> 
       <Skill />
       <Archives />
+      <Project />
     </div>
   );
 }
---
+++ components/Project/Project.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from '../Container';
+import { BentoGrid, BentoGridItem } from '../ui/bento-grid';
+import { items } from '@/lib/ProjectData';
+
+const Project = () => {
+  return (
+    <main className='relative w-full pt-16 pb-20 bg-sky-900' id="Project">
+    <Container>
+      <h1 className='text-3xl underline text-white font-bold pb-10'>Project</h1>
+      <BentoGrid className="max-w-5xl mx-auto md:auto-rows-[20rem]">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            className={item.className}
+            icon={item.icon}
+          />
+        ))}
+      </BentoGrid>
+    </Container>
+    </main>
+  )
+}
+
+
+
+export default Project
