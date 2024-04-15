028a314d5b31b40938028ee564494bdcb74dc7e3
cyj <cyd5538@naver.com>
2024-04-15 12:37:47 +0900

    Feat: about link add

--- components/Home/HomeIntroduce.tsx
+++ components/Home/HomeIntroduce.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../ui/hero-highlight";
+import Link from "next/link";
 
 export function HomeIntroduce() {
   return (
@@ -42,6 +43,26 @@ export function HomeIntroduce() {
         </Highlight>
         {" "}입니다
       </motion.h1>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="flex justify-center mt-12">
+        <Link href="#About">
+          <button className="px-4 py-2 rounded-md border border-black bg-white dark:text-black text-neutarl-700 text-xl dark:hover:shadow-[4px_4px_0px_0px_rgba(255,255,255)] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
+            About Me!
+          </button>
+        </Link>
+      </motion.div>
     </HeroHighlight>
   );
 }
